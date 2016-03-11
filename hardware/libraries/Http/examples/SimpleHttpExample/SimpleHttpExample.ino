6271e325b61bfadda4542dd98472aa0e91a0cf97
william_lai <william_lai@realtek.com>
2016-03-11 10:16:19 +0800

    Title: add missing function implementation

--- hardware/libraries/Http/examples/SimpleHttpExample/SimpleHttpExample.ino
+++ hardware/libraries/Http/examples/SimpleHttpExample/SimpleHttpExample.ino
@@ -112,4 +112,19 @@ void loop()
   while(1);
 }
 
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
 
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
