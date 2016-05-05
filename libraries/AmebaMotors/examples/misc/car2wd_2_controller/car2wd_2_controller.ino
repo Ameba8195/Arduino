7f29b92fe2b41513594b4066dc3f1d15a7a2e32b
william_lai <william_lai@realtek.com>
2016-05-05 15:35:16 +0800

    Title: modify example "car2wd_2_controller.ino"
    Comment: controller 1 can change to use UDP

--- libraries/AmebaMotors/examples/misc/car2wd_2_controller/car2wd_2_controller.ino
+++ libraries/AmebaMotors/examples/misc/car2wd_2_controller/car2wd_2_controller.ino
@@ -4,18 +4,31 @@
 #include <UVC.h>
 
 #define NO_CONTROLLER  0
-#define TCP_CONTROLLER 1
-#define UDP_CONTROLLER 2
+#define CONTROLLER_1   1
+#define CONTROLLER_2   2
 
 char ssid[] = "mycar";        // Set the AP's SSID
 char pass[] = "12345678";     // Set the AP's password
 char channel[] = "11";        // Set the AP's channel
 int status = WL_IDLE_STATUS;  // the Wifi radio's status
 
-WiFiServer server(5001);
-WiFiUDP udp;
+/* control 1 */
+int port_ctrl1 = 5001;
+//#define CTRL1_USE_TCP
+#ifdef CTRL1_USE_TCP
+WiFiServer server(port_ctrl1);
+#else
+// Use UDP in control 1
+WiFiUDP udp1;
+#endif
+
+/* control 2 */
+WiFiUDP udp2;
+int port_ctrl2 = 5002;
 
 Car2wd car(8,9,10,11,12,13);
+
+#define DBG (1)
 
 void setup() {
 
@@ -35,14 +48,20 @@ void setup() {
   while (!UVC.available()) delay(1000);
   Serial.println("UVC is ready");
 
+#ifdef CTRL1_USE_TCP
   server.begin();
   os_thread_create(tcp_ctrl_task, NULL, OS_PRIORITY_REALTIME, 2048);
+#else
+  udp1.begin(port_ctrl1);
+  os_thread_create(udp1_ctrl_task, NULL, OS_PRIORITY_REALTIME, 2048);
+#endif
 
-  udp.begin(5002);
-  os_thread_create(udp_ctrl_task, NULL, OS_PRIORITY_REALTIME, 2048);
+  udp2.begin(port_ctrl2);
+  os_thread_create(udp2_ctrl_task, NULL, OS_PRIORITY_REALTIME, 2048);
 
 }
 
+#ifdef CTRL1_USE_TCP
 uint8_t tcpbuf[256];
 void tcp_ctrl_task(const void *argument) {
   int len;
@@ -56,24 +75,42 @@ void tcp_ctrl_task(const void *argument) {
       memset(tcpbuf, 0, sizeof(tcpbuf));
       len = client.read(tcpbuf, sizeof(tcpbuf)-1);
       if (len > 0) {
+        if (DBG) printf("[TCP]:%s\r\n", tcpbuf);
         tcpbuf[len] = '\0';
       }
-      handleData((const char *)tcpbuf, TCP_CONTROLLER);
+      handleData((const char *)tcpbuf, CONTROLLER_1);
     }
     delay(100);
   }
 }
-
-uint8_t udpbuf[256];
-void udp_ctrl_task(const void *argument) {
+#else
+uint8_t udpbuf1[256];
+void udp1_ctrl_task(const void *argument) {
   int len;
   while(1) {
-    memset(udpbuf, 0, sizeof(udpbuf));
-    len = udp.read(udpbuf, sizeof(udpbuf)-1);
+    memset(udpbuf1, 0, sizeof(udpbuf1));
+    len = udp1.read(udpbuf1, sizeof(udpbuf1)-1);
     if (len > 0) {
-      udpbuf[len] = '\0';
+      if (DBG) printf("[UDP1]:%s\r\n", udpbuf1);
+      udpbuf1[len] = '\0';
     }
-    handleData((const char *)udpbuf, UDP_CONTROLLER);
+    handleData((const char *)udpbuf1, CONTROLLER_1);
+    delay(100);
+  }
+}
+#endif
+
+uint8_t udpbuf2[256];
+void udp2_ctrl_task(const void *argument) {
+  int len;
+  while(1) {
+    memset(udpbuf2, 0, sizeof(udpbuf2));
+    len = udp2.read(udpbuf2, sizeof(udpbuf2)-1);
+    if (len > 0) {
+      if (DBG) printf("[UDP2]:%s\r\n", udpbuf2);
+      udpbuf2[len] = '\0';
+    }
+    handleData((const char *)udpbuf2, CONTROLLER_2);
     delay(100);
   }
 }
