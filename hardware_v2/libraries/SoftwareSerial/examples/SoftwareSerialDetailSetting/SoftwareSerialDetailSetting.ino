60c8316156212c9b20b9955e05ad1a6f62d31f76
william_lai <william_lai@realtek.com>
2017-01-23 16:06:15 +0800

    Title: correct RTS pin in UART detail setting example

--- hardware_v2/libraries/SoftwareSerial/examples/SoftwareSerialDetailSetting/SoftwareSerialDetailSetting.ino
+++ hardware_v2/libraries/SoftwareSerial/examples/SoftwareSerialDetailSetting/SoftwareSerialDetailSetting.ino
@@ -2,13 +2,13 @@
  Software serial test
 
  Receives from serial RX, and then sends to serial TX
- data bits: 7, parity: even, 1 stop bit, with RTS/CTS
+ baud rate: 38400, data bits: 7, parity: even, 1 stop bit, with RTS/CTS
 
  The circuit: (BOARD RTL8195A)
  * RX is digital pin 0 (connect to TX of other device)
  * TX is digital pin 1 (connect to RX of other device)
  * RTS is digital pin 6 (connect to CTS of other device)
- * CTS is digital pin 3 (connect to RTS of other device)
+ * CTS is digital pin 2 (connect to RTS of other device)
 
  The circuit: (BOARD RTL8195A)
  * RX is digital pin 17 (connect to TX of other device)
@@ -38,7 +38,7 @@ void setup() {
   Serial.println("Goodnight moon!");
 
   // set the data rate for the SoftwareSerial port
-  mySerial.begin(38400, 7, PARITY_EVEN, 1, FLOW_CONTROL_RTSCTS, 6, 3);
+  mySerial.begin(38400, 7, PARITY_EVEN, 1, FLOW_CONTROL_RTSCTS, 6, 2);
   mySerial.println("Hello, world?");
 }
 
