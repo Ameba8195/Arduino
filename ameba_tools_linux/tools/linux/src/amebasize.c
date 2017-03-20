24e9834f79ee256feeaa9c4786035e48699b8e16
william_lai <william_lai@realtek.com>
2017-03-20 10:42:16 +0800

    Title: Fix amebasize tool in linux.

--- ameba_tools_linux/tools/linux/src/amebasize.c
+++ ameba_tools_linux/tools/linux/src/amebasize.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
-	sprintf(filename, "%s\\ram_1.p.bin", argv[1]);
+	sprintf(filename, "%s/ram_1.p.bin", argv[1]);
 	FILE* fp_bootloader = fopen(filename, "rb");
 	if (fp_bootloader != NULL) {
 		fseek(fp_bootloader, 0L, SEEK_END);
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
 		printf("bootloader %d\n", size);
 	}
 
-	sprintf(filename, "%s\\ram_2.p.bin", argv[1]);
+	sprintf(filename, "%s/ram_2.p.bin", argv[1]);
 	FILE* fp_img2 = fopen(filename, "rb");
 	if (fp_img2 != NULL) {
 		fseek(fp_img2, 0L, SEEK_END);
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
 		printf("image2 %d\n", size);
 	}
 
-	sprintf(filename, "%s\\ram_3.p.bin", argv[1]);
+	sprintf(filename, "%s/ram_3.p.bin", argv[1]);
 	FILE* fp_img3 = fopen(filename, "rb");
 	if (fp_img3 != NULL) {
 		fseek(fp_img3, 0L, SEEK_END);
